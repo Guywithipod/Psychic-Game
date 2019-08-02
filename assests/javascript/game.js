5869e5b99f125238e52a25667e86b51899a65a4c
Anthony Dominguez <Anthonydv7@outlook.com>
2019-08-02 12:43:12 -0500

    fixed the array that pushes the letters the user choose

--- assests/javascript/game.js
+++ assests/javascript/game.js
@@ -14,20 +14,26 @@ const userChoose = document.getElementById("user-choose");
 
 let computerChoices = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
-//  this is my reset function that should run when guesses left = 0 and should reset the guessed letters
+let guessedLetters = [];
+
+//  this is my reset function that should run when guesses left = 0 and should reset the guessed letters...if i could get the guessed letters part working.
 
 const reset = function () {
     guessesleft = 9;
-    guessedletters = [];
+    guessedLetters = [];
 }
+
 
 
 document.onkeyup = function (event) {
 
-
     const userGuess = event.key;
     //   here the computer grabs a random letter from our computer choices array
     const computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+    
+
+    guessedLetters.push(userGuess)
+    
 
     userChoose.textContent = "you choose: " + userGuess;
 
@@ -50,7 +56,7 @@ document.onkeyup = function (event) {
 
     userWins.textContent = "wins: " + wins;
     userLost.textContent = "losses: " + losses;
-    userChoose.textContent = "you choose: " + userGuess;
+    userChoose.textContent = ("you choose: " + guessedLetters);
     userGuessesLeft.textContent = "guesses left: " + guessesleft;
 
 
