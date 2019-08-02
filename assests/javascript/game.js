b566f2d9864266b4d71e6a458372bc475aee3f86
Anthony Dominguez <Anthonydv7@outlook.com>
2019-08-01 23:29:35 -0500

    Guesses left reset is now working

--- assests/javascript/game.js
+++ assests/javascript/game.js
@@ -12,31 +12,40 @@ const userGuessesLeft = document.getElementById("user-guesses-left");
 
 const userChoose = document.getElementById("user-choose");
 
-let computerChoices = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p","q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+let computerChoices = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
-document.onkeyup = function(event){
+//  this is my reset function that should run when guesses left = 0 and should reset the guessed letters
 
-    
+const reset = function () {
+    guessesleft = 9;
+    guessedletters = [];
+}
+
+
+document.onkeyup = function (event) {
+
+
     const userGuess = event.key;
-      
+    //   here the computer grabs a random letter from our computer choices array
     const computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
 
     userChoose.textContent = "you choose: " + userGuess;
-    
 
-    if (userGuess === computerGuess){
+
+    if (userGuess === computerGuess) {
 
         wins++;
-        userChoose.push(userGuess);
+
     }
 
     else {
         guessesleft--;
     }
 
-    if (guessesleft === 0 ){
+    if (guessesleft === 0) {
         losses++;
-        
+        reset();
+
     }
 
     userWins.textContent = "wins: " + wins;
